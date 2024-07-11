f6cfba2ee73fd0976a69858006cb322328b1aa97
SantoII-github <SantoII1735@gmail.com>
2024-07-10 21:03:15 -0300

    cleanup

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,10 +3,8 @@ function Display(props) {
     return (
         <div>
             <h1>Drum Machine</h1>
-            <p>Click on a drum pad or press the corresponding button to play a sound.</p>
-            <p>Note: At the time of writing the test suite has an issue when ran on Chromium based browsers.
-            <br />Either ignore the <code>The play() request was interrupted by a call to pause().</code> error, or run the test suite on Firefox
-            </p>
+            <p>Click on a drum pad or press the corresponding button to play a sound. <br />
+            Have fun!</p>
             <h2 id="display">{props.lastSound}</h2>
         </div>
     );
